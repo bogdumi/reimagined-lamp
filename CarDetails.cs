28905750eb1c6f51df53ae862643b72221acd6ad
Bogdan Dumitrescu <bogdoomy@gmail.com>
2020-05-21 17:49:10 +0100

    Complete Skeleton for Project

---
+++ CarDetails.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace reimagined_lamp
+{
+    public class CarDetails
+    {
+        public String Make { get; set; }
+
+        public String Model { get; set; }
+
+        public String Colour { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public int MilageAtLastMOT { get; set; }
+    }
+}
---
+++ Controllers/CarDetailsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace reimagined_lamp.Controllers
+{
+    [ApiController]
+    [Route("cardetails")]
+    public class CarDetailsController : ControllerBase
+    {
+        private readonly ILogger<CarDetailsController> _logger;
+
+        public CarDetailsController(ILogger<CarDetailsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public CarDetails Get()
+        {
+            return new CarDetails
+            {
+                Make = "Test",
+                Model = "Test",
+                Colour = "Test",
+                ExpiryDate = DateTime.Now,
+                MilageAtLastMOT = 0
+            };
+        }
+    }
+}
