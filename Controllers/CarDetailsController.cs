019b64f8ab252c746ada78f2173ccb0731195805
Bogdan Dumitrescu <bogdoomy@gmail.com>
2020-05-22 21:06:47 +0100

    Finish Backend

--- Controllers/CarDetailsController.cs
+++ Controllers/CarDetailsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestSharp;
+using System.Text.Json;
 
 namespace reimagined_lamp.Controllers
 {
@@ -20,26 +21,23 @@ namespace reimagined_lamp.Controllers
         }
 
         [HttpGet]
-        public String Get()
-        {
+        public CarDetails Get() {
             return getCarDetails("XX10ABC");
         }
+        
+        [HttpGet("{id}")]
+        public CarDetails Get(String id) {
+            return getCarDetails(id);
+        }
 
-        public String getCarDetails(String reg){
+        public CarDetails getCarDetails(String reg){
             var client = new RestClient("https://beta.check-mot.service.gov.uk/");
             var request = new RestRequest("/trade/vehicles/mot-tests?registration=" + reg);
             request.AddHeader("Accept", "application/json+v6");
             request.AddHeader("x-api-key", "fZi8YcjrZN1cGkQeZP7Uaa4rTxua8HovaswPuIno");
             var response = client.Get(request);
-            var content = response.Content; // Raw content as string
-            return content;
-            // return new CarDetails{
-            //     Make = response.Content.make,
-            //     Model = response.Content.model,
-            //     Colour = ,
-            //     ExpiryDate = ,
-            //     MilageAtLastMOT = 
-            // };
+            var responseData = JsonSerializer.Deserialize<List<CarDetails>>(response.Content);
+            return responseData[0];
         }
     }
 }
